cd078e098b4322dc70cf09d4f9aee83127c9a969
KevinASC5 <KPereira4745@gmail.com>
2018-07-31 08:43:51 -0400

    word

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,8 @@
+let element =document.getElementsByClassName(`right_side`);
+
+element.item(0).style.backgroundColor = '#d3c26f';
+
+
+// let element1 = document.querySelector(name);
+document.getElementsByClassName('name').item(0).style.textShadow="6px 5px 2px #d3c26f ";
+document.getElementsByClassName('name').item(1).style.textShadow="6px 5px 2px #d3c26f ";
---
+++ renameIt/lib/js/main.js
@@ -0,0 +1,3 @@
+ document.getElementById('item_name').textContent="Frostfang"
+
+    document.getElementById('item_img').src="\renameIt\assets\Frostfang_ite
